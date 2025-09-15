8a1f9ae6c25f5d4f31a0b2bb8256f381a929e4db
Adrian Felczak <30607809+solid-rockets@users.noreply.github.com>
2025-09-15 09:30:57 +0900

    GoL (#1)
    * feat: empty file for gol
    
    * feat: gening, randing, and drawing the board works
    
    * feat: implemented the behavior
    
    * feat: refactored some code
    
    * feat: added block padding
    
    * feat: implemented cursor
    
    * feat: added a key to empty the board
    
    * feat: replaced ifs with macro functions as an experiment
    
    * feat: added mouse support
    
    * docs: removed a todo line
    
    * feat: added actually toggling cells
    
    * fix: cursor's size
    
    * docs: updated comments
    
    * feat: added changing fps
    
    * fix: potentially reading outside of the board

---
+++ gol.cpp
@@ -0,0 +1,284 @@
+#include <raylib.h>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+#define INIT_TARGET_FPS 20
+#define CURSOR_DELAY 30
+
+#define BLOCK_PDNG  1
+#define BLOCK_SIZE 20
+#define WIDTH  40
+#define HEIGHT 40
+
+// DEFINITIONS
+enum CELL {
+  EMPTY = 0,
+  ALIVE = 1, //
+  NEXT  = 2  // Gets shifted to alive flag on update.
+};
+
+// GLOBAL STATE
+bool isGameStopped  = false;
+bool mustRandomize  = false;
+bool mustEmpty      = false;
+bool mustToggleCell = false;
+auto fpsCounter     = INIT_TARGET_FPS;
+
+typedef struct {
+  int x;
+  int y;
+  int delay; // Will show cursor for this many frames.
+} CURSOR;
+
+CURSOR cursor = { // Basically, a struct is syn. sugar for a variable prefix.
+  WIDTH  / 2,
+  HEIGHT / 2,
+  0
+};
+
+// LOGIC
+Color Cell2Color(int val) {
+  switch(val) {
+    case CELL::EMPTY : return BLACK;
+    case CELL::ALIVE : return SKYBLUE;
+    default          : return BLACK;
+  }
+}
+
+int CalcPos(int c, int r) {
+  return r * WIDTH + c;
+}
+
+void PopulateBoard(char* board) {
+  for(int r = 0; r < HEIGHT; r++) {
+    for(int c = 0; c < WIDTH; c++) {
+      auto p = CalcPos(c, r);
+      board[p] = CELL::EMPTY;
+    }
+  }
+}
+
+void RandomizeBoard(char* board) {
+  mustRandomize = false;
+
+  for(int r = 0; r < HEIGHT; r++) {
+    for(int c = 0; c < WIDTH; c++) {
+      auto p = CalcPos(c, r);
+
+      if(rand() % 2)
+        board[p] = CELL::EMPTY;
+      else
+        board[p] = CELL::ALIVE;
+    }
+  }
+}
+
+void EmptyBoard(char* board) {
+  mustEmpty = false;
+
+  for(int r = 0; r < HEIGHT; r++) {
+    for(int c = 0; c < WIDTH; c++) {
+      auto p = CalcPos(c, r);
+
+      board[p] = CELL::EMPTY;
+    }
+  }
+}
+
+void DrawBoard(char* board) {
+  for(int r = 0; r < HEIGHT; r++) {
+    for(int c = 0; c < WIDTH; c++) {
+      auto p = CalcPos(c, r);
+      auto color = Cell2Color(board[p]);
+
+      auto posX = c * BLOCK_SIZE;
+      auto posY = r * BLOCK_SIZE;
+
+      DrawRectangle(
+        posX + BLOCK_PDNG, // Block padding added.
+        posY + BLOCK_PDNG,
+        BLOCK_SIZE - BLOCK_PDNG * 2,
+        BLOCK_SIZE - BLOCK_PDNG * 2,
+        color);
+    }
+  }
+}
+
+void UpdateCursor(int dx, int dy) {
+  cursor.x += dx;
+  cursor.y += dy;
+
+  if(cursor.x < 0) cursor.x = 0;
+  if(cursor.y < 0) cursor.y = 0;
+  if(cursor.x >= WIDTH)  cursor.x = WIDTH  - 1;
+  if(cursor.y >= HEIGHT) cursor.y = HEIGHT - 1;
+
+  cursor.delay = CURSOR_DELAY;
+}
+
+void UpdateCursorFromCoords() {
+  auto x = GetMouseX() / BLOCK_SIZE;
+  auto y = GetMouseY() / BLOCK_SIZE;
+
+  auto delta_x = x - cursor.x;
+  auto delta_y = y - cursor.y;
+
+  UpdateCursor(delta_x, delta_y);
+}
+
+void ToggleCell(char* board) {
+  auto p = CalcPos(cursor.x, cursor.y);
+
+  // CELL::ALIVE is actually a flag - use to filter specifically for that value.
+  board[p] = board[p] & CELL::ALIVE ? CELL::EMPTY : CELL::ALIVE;
+  mustToggleCell = false;
+
+  cursor.delay = CURSOR_DELAY;
+}
+
+void DrawCursor() {
+  if(!cursor.delay) return;
+  // DrawRectangleLines makes the lines bigger than necessary.
+  DrawRectangle(
+    cursor.x * BLOCK_SIZE,
+    cursor.y * BLOCK_SIZE,
+    BLOCK_SIZE,
+    BLOCK_SIZE,
+    ORANGE
+  );
+}
+
+void UpdateFPS(int diff) {
+  fpsCounter += diff;
+
+  if(fpsCounter < 1)
+    fpsCounter = 1;
+
+  SetTargetFPS(fpsCounter);
+}
+
+void ProcessInputs() {
+  #define CHECK_KEY(key, action) if(IsKeyPressed(key)) action
+  CHECK_KEY(KEY_W, UpdateCursor( 0,-1);)
+  CHECK_KEY(KEY_S, UpdateCursor( 0, 1);)
+  CHECK_KEY(KEY_A, UpdateCursor(-1, 0);)
+  CHECK_KEY(KEY_D, UpdateCursor( 1, 0);)
+
+  CHECK_KEY(KEY_R, mustRandomize = true;)
+  CHECK_KEY(KEY_E, mustEmpty = true;)
+
+  CHECK_KEY(KEY_ENTER, mustToggleCell = true;)
+  CHECK_KEY(KEY_SPACE,
+    isGameStopped = !isGameStopped;)
+
+  CHECK_KEY(KEY_LEFT_BRACKET,  UpdateFPS(-1);)
+  CHECK_KEY(KEY_RIGHT_BRACKET, UpdateFPS(+1);)
+  #undef CHECK_KEY
+
+  // Mouse section.
+  auto mouseDelta = GetMouseDelta();
+  if(mouseDelta.x || mouseDelta.y) // both are floats, but should be 0.0
+    UpdateCursorFromCoords();
+
+  if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+    mustToggleCell = true;
+}
+
+void UpdateSingleCell(char* board, int p) {
+  int isL = p % WIDTH == 0;
+  int isR = p % WIDTH == WIDTH - 1;
+  int isU = p < WIDTH;
+  int isD = p > WIDTH * (HEIGHT - 1);
+
+  int count = 0;
+
+  // For reference:
+  // 123
+  // 4p5
+  // 678
+  // NOTE: I could refactor using a macro, but using ifs is much more clear.
+  if(!isL && !isU) count +=  (board[p - WIDTH - 1] & CELL::ALIVE);
+  if(        !isU) count +=  (board[p - WIDTH    ] & CELL::ALIVE);
+  if(!isR && !isU) count +=  (board[p - WIDTH + 1] & CELL::ALIVE);
+
+  if(!isL        ) count +=  (board[p         - 1] & CELL::ALIVE);
+  if(!isR        ) count +=  (board[p         + 1] & CELL::ALIVE);
+
+  if(!isL && !isD) count +=  (board[p + WIDTH - 1] & CELL::ALIVE);
+  if(        !isD) count +=  (board[p + WIDTH    ] & CELL::ALIVE);
+  if(!isR && !isD) count +=  (board[p + WIDTH + 1] & CELL::ALIVE);
+
+  auto cellVal = board[p];
+
+  if(cellVal == CELL::EMPTY) {
+    if(count == 3)
+      board[p] |= CELL::NEXT;
+  } else if(cellVal == CELL::ALIVE ) {
+    if(count == 2 || count == 3)
+      board[p] |= CELL::NEXT;
+  }
+}
+
+void UpdateCells(char* board) {
+  for(int r = 0; r < HEIGHT; r++) {
+    for(int c = 0; c < WIDTH; c++) {
+      auto p = CalcPos(c, r);
+
+      UpdateSingleCell(board, p);
+    }
+  }
+}
+
+void AdvanceCells(char* board) {
+  auto totalSize = HEIGHT * WIDTH;
+  for(int i = 0; i < totalSize; i++)
+    board[i] >>= 1; // NEXT state flows down to LIFE flag.
+}
+
+void UpdateMisc() {
+  if(cursor.delay > 0) cursor.delay--;
+}
+
+int main() {
+  // Initialize all variables.
+  srand(time(0));
+  auto board = new char[WIDTH * HEIGHT];
+  PopulateBoard(board);
+
+  // Proceed with drawing, etc.
+  InitWindow(BLOCK_SIZE * WIDTH, BLOCK_SIZE * HEIGHT, "gol");
+  SetTargetFPS(fpsCounter);
+
+  // Game loop
+  while(!WindowShouldClose()) {
+    BeginDrawing();
+      ClearBackground(BLACK);
+      DrawCursor(); // Before board because of the cursor's DrawRectangle.
+      DrawBoard(board);
+    EndDrawing(); // This polls events automatically.
+
+    ProcessInputs();
+    UpdateMisc();
+
+    if(mustToggleCell)
+      ToggleCell(board);
+
+    if(mustRandomize)
+      RandomizeBoard(board);
+
+    if(mustEmpty)
+      EmptyBoard(board);
+
+    if(!isGameStopped) {
+      UpdateCells(board);
+      AdvanceCells(board);
+    }
+  }
+
+  // Cleanup.
+  CloseWindow();
+  delete board; // PREVENT MEMORY LEAKS!!!
+
+  return 0;
+}
