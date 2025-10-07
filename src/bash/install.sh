af78650f3792a09bd7b00d6fad2b11353258dd45
AMF <af.dev@protonmail.com>
2025-10-07 23:02:16 +0900

    added the install script

---
+++ src/bash/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Updates the lists.
+sudo apt update
+
+# Installs essential system stuff and compilers.
+sudo apt install -y build-essential
+sudo apt install -y clamav
+sudo apt install -y git
+sudo apt install -y tree
+sudo apt install -y hexedit
+
+# Install other dev tools
+sudo apt install -y chicken-bin
+
+# Installs essential system stuff and compilers.
+#sudo apt install -y audacity
+#sudo apt install -y lmms
+
+#sudo apt install -y blender
+#sudo apt install -y openscad
+
+#sudo apt install -y gimp
+#sudo apt install -y inkscape
