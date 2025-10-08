74bcd509e7a17cff554ed9401ff13c89ca71fcf8
AMF <af.dev@protonmail.com>
2025-10-08 18:51:28 +0900

    added to install script

--- src/bash/install.sh
+++ src/bash/install.sh
@@ -12,6 +12,8 @@ sudo apt install -y hexedit
 
 # Install other dev tools
 sudo apt install -y chicken-bin
+sudo apt install -y verilator
+sudo apt install -y iverilog
 
 # Installs essential system stuff and compilers.
 #sudo apt install -y audacity
